01cb20d9500989c4ee3deb39c700b405c617c7c7
YOGEESH K P <yogeesh793@gmail.com>
2024-10-26 10:38:34 +0530

    Add files via upload

---
+++ Conditional_Statement.py
@@ -0,0 +1,14 @@
+# This is take 2 input for user
+a=int(input("Enter first value: "))
+b=int(input("Enter second value: "))
+if a>b:
+    print("a is greater")
+    print("--------------------")
+
+
+# write a python program for executed for pass marks
+# [marks is above 39 is pass]
+marks = float(input("Enter your marks: ")) 
+if marks>39:
+    print("pass") 
+      
